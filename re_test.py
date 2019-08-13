16188f4c0a2abee4d33d90518b60a4a9ad1458d8
diving16 <daiweiying@seu.edu.cn>
2019-08-13 09:43:19 +0800

    Create re_test.py

---
+++ re_test.py
@@ -0,0 +1,32 @@
+import re
+
+message='Hi,my name is Meiqi Meng and my wechat id is MeiqiMeng,\
+you can call me at (86)-18888888888,if it doesn\'t work, you can \
+call Meiqi77 by 86-16666666666'
+
+#find_phone_number=re.compile(r'\d\d-\d\d\d\d\d\d\d\d\d\d\d')
+
+# =============================================================================
+# #Tips:分成前后两块
+# find_phone_number=re.compile(r'\((\d\d)\)-(\d\d\d\d\d\d\d\d\d\d\d)')
+# 
+# #找到第一个符合条件的
+# match_object=find_phone_number.search(message)
+# 
+# # =============================================================================
+# # #找到所有符合的元素，放在一个列表里
+# # match_object=find_phone_number.findall(message)
+# # 
+# # =============================================================================
+# print(match_object)
+# 
+# #在search时使用
+# print(match_object.group())
+# 
+# =============================================================================
+
+Meiqi_Regex=re.compile(r'Meiqi(Meng|77)')
+match_object=Meiqi_Regex.search(message)
+
+
+print(match_object.group())
